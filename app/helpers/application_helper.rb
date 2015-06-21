79887f2b38f351199221816b4608ec5a95293b08
Yannis Jaquet <yannis.jaquet@unige.ch>
2015-06-21 21:25:59 +0200

    improves styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,8 @@ module ApplicationHelper
     link << link_to("cancel", (session[:return_to].nil? ? root_path : session[:return_to]), accesskey: 'ESC', title: "Cancel #{f.object_name} form", class: "cancel #{request.format == 'application/javascript' ? 'close_div' : ''}")
     link.join(' ').html_safe
   end
+
+  def link_to_back(default=root_path, options={})
+    link = link_to('<span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> back'.html_safe, default, options)
+  end
 end
